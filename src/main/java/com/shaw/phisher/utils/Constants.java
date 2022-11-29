dead715238b36068baaed207bce8b33e3011c59a
har-shaw <8888harsha4444@gmail.com>
2022-11-29 13:07:07 -0500

    remove: pinterest, snapchat, spotify

--- src/main/java/com/shaw/phisher/utils/Constants.java
+++ src/main/java/com/shaw/phisher/utils/Constants.java
@@ -4,5 +4,5 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
-    public static final List<String> options = Arrays.asList("Facebook", "Discord", "Instagram", "Google", "Microsoft", "Twitch", "DropBox", "Pinterest", "Github", "StackoverFlow", "Gitlab", "Adobe", "Twitter", "Spotify", "Steam", "Linkedin", "Snapchat", "Ebay", "Netflix");
+    public static final List<String> options = Arrays.asList("Facebook", "Discord", "Instagram", "Google", "Microsoft", "DropBox", "Github", "StackoverFlow", "Gitlab", "Adobe", "Twitter", "Spotify", "Steam", "Linkedin", "Ebay", "Netflix");
 }
